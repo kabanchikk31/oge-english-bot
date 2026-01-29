7374d3fe6e7901177d9113fbc02247116b9d1592
kabanchikk31 <niktodlyatibiyashi@gmail.com>
2026-01-30 01:53:08 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -108,5 +108,5 @@ async def process_answer(callback_query: types.CallbackQuery):
         )
 
 
-if name == "__main__":
+if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
